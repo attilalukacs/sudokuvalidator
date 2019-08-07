c44bccf20285adf9cea7727012724897fc56b834
Attila Lukács <attila.lukacs@protonmail.ch>
2019-08-07 16:52:10 +0200

    add maven project

---
+++ src/main/java/hu/procyon/sudokuvalidator/App.java
@@ -0,0 +1,9 @@
+package hu.procyon.sudokuvalidator;
+
+public class App {
+    public static void main(String[] args) {
+        for (String arg: args) {
+            System.out.println(arg);
+        }
+    }
+}
