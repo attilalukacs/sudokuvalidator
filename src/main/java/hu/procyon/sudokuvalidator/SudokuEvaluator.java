ebcb601323b19b8d5d8a7740c04b941af202a43a
Attila Lukács <attila.lukacs@protonmail.ch>
2019-08-10 07:53:59 +0200

    modify evaluator to only try one coordinate

--- src/main/java/hu/procyon/sudokuvalidator/SudokuEvaluator.java
+++ src/main/java/hu/procyon/sudokuvalidator/SudokuEvaluator.java
@@ -1,9 +1,6 @@
 package hu.procyon.sudokuvalidator;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -11,21 +8,23 @@ import java.util.NoSuchElementException;
  */
 public class SudokuEvaluator {
 
-    public List<SudokuCoordinate> findEmptyCoordinates(SudokuState state) {
-        List<SudokuCoordinate> result = new ArrayList<>();
+    public SudokuCoordinate findLeastAmbigousEmptyCoordinates(SudokuState state) {
+        SudokuCoordinate result = null;
+        int noPossibleDigits = Integer.MAX_VALUE;
         for (int row = 0; row < state.getSize(); row++) {
             for (int column = 0; column < state.getSize(); column++) {
                 if (!state.isDigitSet(row, column)) {
-                    if (state.getNumberOfPossibleDigits(row, column) > 0) {
-                        result.add(new SudokuCoordinate(row, column));
+                    int tempNoDigits = state.getNumberOfPossibleDigits(row, column);
+                    if (tempNoDigits < noPossibleDigits) {
+                        result = new SudokuCoordinate(row, column);
+                        noPossibleDigits = tempNoDigits;
                     }
-                    else {
-                        return Collections.emptyList();
+                    if (tempNoDigits < 1) {
+                        return result;
                     }
                 }
             }
         }
-        result.sort((c1, c2) -> Integer.compare(noSteps(state, c1), noSteps(state, c2)));
         return result;
     }
 
@@ -34,54 +33,43 @@ public class SudokuEvaluator {
     }
 
     public Iterator<SudokuStep> getStepIterator(SudokuState state) {
-        List<SudokuCoordinate> coordinates = findEmptyCoordinates(state);
-        return new SudokuStepIterator(state, coordinates);
+        SudokuCoordinate coordinate = findLeastAmbigousEmptyCoordinates(state);
+        return coordinate != null && 1 <= noSteps(state, coordinate)
+            ? new SudokuStepIterator(state, coordinate)
+            : null;
     }
 
     private class SudokuStepIterator implements Iterator<SudokuStep> {
-        private final Iterator<SudokuCoordinate> coordIter;
         private final SudokuState state;
-        private SudokuCoordinate coord;
-        private List<Integer> availableDigits;
-        private int digitIndex;
+        private final SudokuCoordinate coord;
+        private int nextDigit;
 
-        private SudokuStepIterator(SudokuState state, List<SudokuCoordinate> coordinates) {
-            this.coordIter = new ArrayList<>(coordinates).iterator();
+        private SudokuStepIterator(SudokuState state, SudokuCoordinate coordinate) {
             this.state = state;
-            this.coord = null;
-            this.availableDigits = null;
-            this.digitIndex = 0;
+            this.coord = coordinate;
+            this.nextDigit = 0;
+            findNext();
+        }
+
+        private void findNext() {
+            do {
+                nextDigit++;
+            } while (nextDigit <= state.getSize() && state.isDigitExcluded(coord.row, coord.column, nextDigit));
         }
 
         @Override
         public boolean hasNext() {
-            return (coord != null && digitIndex < availableDigits.size()) || coordIter.hasNext();
+            return nextDigit <= state.getSize();
         }
 
         @Override
         public SudokuStep next() {
-            if ((coord == null || availableDigits.size() <= digitIndex) && coordIter.hasNext()) {
-                coord = coordIter.next();
-                availableDigits = getAvailableDigits(coord);
-                digitIndex = 0;
-            }
-            if (coord != null && digitIndex < availableDigits.size()) {
-                SudokuStep step = new SudokuStep(coord, availableDigits.get(digitIndex), availableDigits.size() < 2);
-                digitIndex++;
-                return step;
+            if (hasNext()) {
+                int digit = nextDigit;
+                findNext();
+                return new SudokuStep(coord, digit, hasNext()); //TODO: hasNext() is not exactly single
             }
             throw new NoSuchElementException();
         }
-
-		private List<Integer> getAvailableDigits(SudokuCoordinate coordinate) {
-            List<Integer> result = new ArrayList<>();
-            for (int digit = 1; digit <= state.getSize(); digit++) {
-                if (state.canSetDigit(coordinate.row, coordinate.column, digit)) {
-                    result.add(digit);
-                }
-            }
-			return result;
-		}
-
     }
 }
