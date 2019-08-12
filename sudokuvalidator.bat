c8b112053cf7c8b09817bf78067d31125d34bcd0
Attila Lukács <attila.lukacs@protonmail.ch>
2019-08-12 12:53:35 +0200

    fix Windows batch

--- sudokuvalidator.bat
+++ sudokuvalidator.bat
@@ -1,4 +1,7 @@
-SET JAR_DIR=target
+@ECHO OFF
 
-java -jar %JAR_DIR%\sudokuvalidator-${VERSION}.jar $*
+SET JAR_DIR=target
+SET VERSION=1.0-SNAPSHOT
+
+java -jar %JAR_DIR%\sudokuvalidator-%VERSION%.jar %*
 EXIT /B %ERRORLEVEL%
